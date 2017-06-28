b2db85d47fa5a7add067a51777070cbff9e2d2f5
Lisa Schoofs <lisaschoofs@gmail.com>
2017-06-28 16:45:06 -0500

    intial commit

---
+++ Game.java
@@ -0,0 +1,8 @@
+class Game {
+  private String answer;
+
+  //constructor
+  public Game(String answer) {
+    this.answer = answer;
+  }
+}
---
+++ Hangman.java
@@ -0,0 +1,8 @@
+public class Hangman {
+
+  public static void main(String[] args) {
+
+    Game game = new Game("example");
+  }
+
+}

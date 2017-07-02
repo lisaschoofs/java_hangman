ef6eb33b124d30f04badeb9e0a7069356d852e71
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-01 21:21:36 -0500

    adds prompt for guess

--- Hangman.java
+++ Hangman.java
@@ -3,6 +3,12 @@ public class Hangman {
   public static void main(String[] args) {
 
     Game game = new Game("example");
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+      System.out.println("We got a hit!");
+    } else {
+      System.out.println("Ooops - missed!");
+    }
   }
-
 }

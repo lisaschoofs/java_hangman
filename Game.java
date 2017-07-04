97231b25625bebdb3cef94efad0d9e27567d74d1
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-03 23:40:15 -0500

    adds max tries, displays remaining guesses

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 class Game {
+  public static final int MAX_MISSES = 7;
   private String answer;
   private String hits;
   private String misses;
@@ -18,6 +19,10 @@ class Game {
       misses += letter;
     }
      return isHit;
+  }
+
+  public int getRemainingTries() {
+    return MAX_MISSES - misses.length();
   }
 
   public String getCurrentProgress(){
--- Hangman.java
+++ Hangman.java
@@ -4,13 +4,11 @@ public class Hangman {
 
     Game game = new Game("example");
     Prompter prompter = new Prompter(game);
-    prompter.displayProgress();
-    boolean isHit = prompter.promptForGuess();
-    if (isHit) {
-      System.out.println("We got a hit!");
-    } else {
-      System.out.println("Ooops - missed!");
+
+    while (game.getRemainingTries() > 0) {
+      prompter.displayProgress(),
+      prompter.promptForGuess();
     }
-    prompter.displayProgress();
+
   }
 }

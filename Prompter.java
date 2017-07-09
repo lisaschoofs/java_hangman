dbc2831a259ed5a0210154e0f6477a48df78f92f
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-08 19:45:30 -0500

    switches all guesses to lowercase

--- Prompter.java
+++ Prompter.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 class Prompter {
   private Game game;
 
-   public Prompter(Game game) {
-    this.game = game;
-   }
+  public Prompter(Game game) {
+   this.game = game;
+  }
 
-   public boolean promptForGuess() {
-     Scanner scanner = new Scanner(System.in);
-     System.out.print("Enter a letter:  ");
-     String guessInput = scanner.nextLine();
-     char guess = guessInput.charAt(0);
-     boolean isHit = false;
+  public boolean promptForGuess() {
+    Scanner scanner = new Scanner(System.in);
+    boolean isHit = false;
+    boolean isAcceptable = false;
+
+    do {
+      System.out.print("Enter a letter:  ");
+      String guessInput = scanner.nextLine();
+      char guess = guessInput.charAt(0);
+
       try {
-        game.applyGuess(guess);
+        isHit = game.applyGuess(guess);
+        isAcceptable = true;
       } catch(IllegalArgumentException iae) {
         System.out.println(iae.getMessage());
       }
-      return isHit;
-    }
+    } while(! isAcceptable);
+    return isHit;
+  }
 
-   public void displayProgress(){
-     System.out.printf("You have %d tries left to solve:  %s%n",
-                       game.getRemainingTries();
-                       game.getCurrentProgress());
-   }
+  public void displayProgress(){
+    System.out.printf("You have %d tries left to solve:  %s%n",
+                      game.getRemainingTries(),
+                      game.getCurrentProgress());
+  }
 
 }

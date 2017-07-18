b16b2f52657342c0a818fd348d9c40e5589c8290
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-17 23:05:11 -0500

    validation to require answer string

--- Hangman.java
+++ Hangman.java
@@ -1,8 +1,12 @@
 public class Hangman {
 
   public static void main(String[] args) {
-
-    Game game = new Game("example");
+    if (args.length == 0) {
+      System.out.println("Usage: java Hangman <answer>");
+      System.err.println("answer is required.");
+      System.exit(1);
+    }
+    Game game = new Game(args[0]);
     Prompter prompter = new Prompter(game);
 
     while (game.getRemainingTries() > 0 && !game.isWon()) {
